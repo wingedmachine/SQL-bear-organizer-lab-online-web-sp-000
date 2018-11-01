f389a1669f98f5d8326e18d69aa0a7c538f75377
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 19:26:25 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Mr. Chocolate", 17, "M", "chocolate", "melty", true);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Rowdy", 5, "F", "blue", "rowdy", true);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Tabitha", 800, "F", "black", "cursed", false);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Sergeant Brown", 18, "M", "navy", "military", true);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Melissa", 23, "F", "toast", "dogs", true);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Grinch", 50, "M", "green", "grinchy", true);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Wendy", 12, "F", "red", "flying", true);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES (NULL, 13, "M", "blood", "hungry", false);
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES ("Mr. Chocolate", 17, "M", "chocolate", "melty", true);
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES ("Rowdy", 5, "F", "blue", "rowdy", true);
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES ("Tabitha", 800, "F", "black", "cursed", false);
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES ("Sergeant Brown", 18, "M", "navy", "military", true);
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES ("Melissa", 23, "F", "toast", "dogs", true);
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES ("Grinch", 50, "M", "green", "grinchy", true);
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES ("Wendy", 12, "F", "red", "flying", true);
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES (NULL, 13, "M", "blood", "hungry", false);
