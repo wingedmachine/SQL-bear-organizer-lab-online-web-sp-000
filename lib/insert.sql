0bd194cfe23b0c01cb1baaaa1b70b4f01f3aed19
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 19:20:54 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ("Mr. Chocolate");
+INSERT INTO bears ("Rowdy");
+INSERT INTO bears ("Tabitha");
+INSERT INTO bears ("Sergeant Brown");
+INSERT INTO bears ("Melissa");
+INSERT INTO bears ("Grinch");
+INSERT INTO bears ("Wendy");
+INSERT INTO bears (NULL);
