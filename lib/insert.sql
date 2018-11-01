5cd954d275af15ead885e3ee4d1570ec430daca3
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 19:24:21 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears ("Mr. Chocolate");
-INSERT INTO bears ("Rowdy");
-INSERT INTO bears ("Tabitha");
-INSERT INTO bears ("Sergeant Brown");
-INSERT INTO bears ("Melissa");
-INSERT INTO bears ("Grinch");
-INSERT INTO bears ("Wendy");
-INSERT INTO bears (NULL);
+INSERT INTO bears ("Mr. Chocolate", 17, "M", "chocolate", "melty", true);
+INSERT INTO bears ("Rowdy", 5, "F", "blue", "rowdy", true);
+INSERT INTO bears ("Tabitha", 800, "F", "black", "cursed", false);
+INSERT INTO bears ("Sergeant Brown", 18, "M", "navy", "military", true);
+INSERT INTO bears ("Melissa", 23, "F", "toast", "dogs", true);
+INSERT INTO bears ("Grinch", 50, "M", "green", "grinchy", true);
+INSERT INTO bears ("Wendy", 12, "F", "red", "flying", true);
+INSERT INTO bears (NULL, 13, "M", "blood", "hungry", false);
