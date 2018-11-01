3b3bd05d78821395db4d784410a11055afd3e1e7
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 19:34:34 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,29 +3,29 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-
+  "SELECT name FROM bears ORDER BY name DESC"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-
+  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-
+  "SELECT COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-
+  "SELECT COUNT(id) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-
+  "SELECT * FROM bears WHERE name is NULL"
 end
