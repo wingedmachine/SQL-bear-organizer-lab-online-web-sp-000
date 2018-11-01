fc3679443d085f22ef8fbf827c6cccf8be23343e
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 19:30:14 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,31 @@
 def selects_all_female_bears_return_name_and_age
-  
+  "SELECT name, age FROM bears WHERE gender = 'F'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  
+
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  
+
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  
+
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  
+
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  
+
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  
+
 end
 
 def selects_bear_that_killed_Tim
-  
+
 end
